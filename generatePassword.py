f3b670053f864b220b9bbf434892cf3ec66dab97
Deepak <raydeepak4596@gmail.com>
2022-11-23 10:24:27 +0530

    thirdcommit

--- generatePassword.py
+++ generatePassword.py
@@ -7,14 +7,24 @@ random.shuffle(ss)
 
 l=[]
 
+while True:
+    
 
+    inp=int(input('''
+    1  enter  password length
+    2  exit'''))
 
-inp=int(input("Enter password length  :"))
-for i in range(inp):
-    l.append(random.choice(ss))
-l="".join(l)
-print(l)
-print("password Generate")
+    if(inp == 1):
+
+        for i in range(inp):
+            l.append(random.choice(ss))
+        l="".join(l)
+        print(l)
+    elif(inp == 2):
+        break
+    else:
+        print("invalid number")
+       
 
 
 
